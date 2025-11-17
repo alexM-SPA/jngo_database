5def537db9083c79b66966fddfbc7b160ee458ad
alexM-SPA <156834565+alexM-SPA@users.noreply.github.com>
2025-11-17 16:23:27 -0500

    Create welcome.py

---
+++ welcome.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title('American Jewish Institutions')
+st.text('A business and financial analysis of American Jewish institutions, '
+        'using data from non-profits, grantmakers, and universities'
+        ' driving the American Jewish community today.'
+        )
+st.text('This project is led by by Mark Kahan at American University'
+        " with research assistants Alex Moskovitz (MS '26) and Leonard Schneider (MBA '26)."
+        )
+st.text('Data featured on this dashboard was created using data from the Department of Education and Internal Revenue Service, '
+        'incorporating existing research from Dr. Jack Wertheimer (Jewish Theological Seminary) with the aid of a variety of artificial intelligence tools.')
+
+st.subheader('Disclaimer:')
+st.text('While every effort has been made to compile complete and accurate data, '
+        ' all quantitative information should be regarded as estimates with potential for significant error. '
+        ' Material amounts of charitable donations flow from donor advised funds (DAFs) identifiable, if at all, by the transmitting financial institution. '
+        ' Reporting to regulatory authorities can be affected by differences in institutional capabilities and compliance disposition.')
